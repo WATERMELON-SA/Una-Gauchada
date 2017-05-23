5decc2ea5a735a2398a256af024fb580efd008db
santit96 <santi.tettamanti96@gmail.com>
2017-05-23 00:39:00 -0300

    Add files via upload

---
+++ comprarCreditos.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html  style="overflow-x: hidden">
+<head>
+	<link rel="shortcut icon" href="logo.png">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="logo.png">
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="Roboto-Regular.ttf">
+	<link rel="stylesheet" type="text/css" href="estilo1.css">	
+	<script type="text/javascript" src="jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="bootstrap.min.js"></script>
+	<title>Una Gauchada</title>
+</head>
+
+<header>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+  <a href="index.php"><img alt="brand" class="navbar-left" src="logo.png" style="width: 50px; height: 50px"></a>
+
+
+   <form class="navbar-form navbar-left" role="search">
+  
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="Buscar"> 
+  </div>
+  <button type="submit" class="btn btn-default"> <img src="glyphicons-28-search.png"></button>
+</form>
+
+	<button class="btn btn-default navbar-right navbar-btn" type="button" >
+    	<a href="index.php"> Inicio</a>
+  </button>
+
+
+	<button type="button" class="btn btn-default navbar-btn navbar-right"><a href='#'> MiPerfil</a></button>
+
+
+  </div>
+</nav>
+
+		<img style="width: 100%;" src="banner.png">
+
+
+</header>
+<body>
+	
+<header style="margin-left: 25%"><h2>Compra créditos</h2>
+<h3>Precio unitario del crédito: $50</h3>
+</header>
+<div style="text-align: center; margin-top: 3%">
+	<h4>Cantidad:  <input type="tel" name="Cantidad"></h4>  
+	<button>Comprar!</button>
+</div>
+
+</body>
