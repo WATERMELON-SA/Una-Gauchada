2644c4bd2ffd15d6cb578ec7df7a0a56045f560a
JulianPasquale <julianagustin78@gmail.com>
2017-07-18 20:27:15 -0300

    Add files via upload

--- adminReputaciones.php
+++ adminReputaciones.php
@@ -2,69 +2,136 @@
 	include "navbaravanzado.php";
 	include "conexion.php";
 	$conection = conectar();
-	$reputaciones = $conection -> query("SELECT * FROM reputacion ORDER BY valor_min");
-	$total = $conection -> query("SELECT sum(valor_max-valor_min) as suma FROM reputacion ORDER BY valor_min");
-	$total= $total -> fetch_assoc();
+	include "modificarReputacion.php";
+ 	if (isset($_POST['nombre'])) {
+ 		modificarReputacion($conection);
+ 	}
+ 	if (isset($_POST['nuevoNombre'])) {
+ 		$nuevoNombre=$_POST['nuevoNombre'];
+ 		$nuevoColor=$_POST['nuevoColor'];
+ 		$nuevoValor_max=$_POST['nuevoValor_max'];
+ 		$conection-> query("INSERT INTO reputacion (nombre, color, valor_max) VALUES ('$nuevoNombre','$nuevoColor','$nuevoValor_max')");
+ 	}
+	$reputaciones = $conection -> query("SELECT * FROM reputacion ORDER BY valor_max");
 	$arreglo= $reputaciones-> fetch_assoc();
-	$colores = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Beige","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
-	$i = 0;
-	$j= -1;
+	$i = -1;
+	$ant=0;
+	$total=0;
 ?>
 <br>
-<div class="container">
+<div class="container-fluid">
 <div  class="progress" style="height: 60px;">
+
 <?php  	
 	while (isset($arreglo)){
-		$j= $j + 1;
-		$diferencia = $arreglo['valor_max'] - $arreglo['valor_min'];
-		$diferencia = 100 * $diferencia / $total['suma'];
-?>
-
-  <div class="progress-bar" title="<?php echo $arreglo['nombre']; ?>" style="width: <?php echo $diferencia ?>%; background-color: <?php echo $colores[$i]; if ($i < sizeof($colores)) $i=$i +1; else $i=1; ?>">
-  <b style="color:black;">
-  <?php //echo $arreglo['nombre']; ?>
-  <!--<br>-->
-  <?php 
-  	$rangos[$j]= $arreglo['valor_min']." a ".$arreglo['valor_max'];
-  	echo $rangos[$j];
-  	$arreglo2[$j]= $arreglo['nombre'];
-  ?>
-  </b>
-  </div>
-  <?php 
+		$i=$i+1;
+		if ($ant==0)
+			$valorMinimo=-9999;
+		else 
+			$valorMinimo= $ant['valor_max']+1;
+	  	$rangos[$i]= $valorMinimo." a ".$arreglo['valor_max'];
+	  	$arreglo2[$i]= $arreglo['nombre'];
+	  	$diferencia[$i] = $arreglo['valor_max'] - $valorMinimo;
+	  	if ($diferencia[$i]>200) {
+	  		$diferencia[$i]=200;
+	  	}
+	  	$colores[$i]=$arreglo['Color'];
+	  	$total = $total + $diferencia[$i];
+	  	$id[$i]=$arreglo['idReputacion'];
+	  	$ant=$arreglo;
   		$arreglo = $reputaciones-> fetch_assoc();
 	}
+	for ($j=0; $j <=$i ; $j++) {
 	?>
+		<div class="progress-bar" title="<?php echo $arreglo2[$j]; ?>" style="cursor:pointer; width: <?php echo (100 * $diferencia[$j] / $total) ?>%; background-color: <?php echo $colores[$j];?>">
+			<b style="color: black;"> <?php echo $rangos[$j]; ?> </b>
+		</div>
+ 	<?php
+ 	}
+ 	?>
 </div>
 
-	<div class="col-sm-6 col-sm-offset-3" style="align-self: center;">
-	<div style="width: 70%; text-align: center;" class="panel panel-default panel-center">
-  <!-- Default panel contents -->
-  <div class="panel-heading">Reputaciones</div>
 
-  <!-- Table -->
-  <table class="table">
-		<tr>
-			<th>Nombre reputacion</th>
-			<!--<th>Color</th>-->
-			<th>Rango</th>
-		</tr>
-		
-		<?php
-			for ($j; $j >=0 ; $j--) { 
-		?>
+	<div class="col-sm-6 col-sm-offset-4" style="align-self: center;">
+		<div style="width: 70%; text-align: center;" class="panel panel-default panel-center">
+	  <!-- Default panel contents -->
+	  <div class="panel-heading">Reputaciones</div>
+
+	  <!-- Table -->
+	  <table class="table">
 			<tr>
-			<td style="background-color:<?php echo $colores[$j];?> "><?php echo $arreglo2[$j];?></td>
-			<!--<td style="background-color:<?php echo $colores[$j];?> "></td><-->
-			<td><?php echo $rangos[$j]; ?></td>
+				<th>Nombre reputacion</th>
+				<th>Rango</th>
+				<th></th>
 			</tr>
-		<?php
-			}
-		?>
-		
-	</table>  
+			<?php
+				for ($j=0; $j <=$i ; $j++) { 
+			?>
+				<tr>
+				<td style="background-color:<?php echo $colores[$j];?> "><?php echo $arreglo2[$j];?></td>
+				<td><?php echo $rangos[$j]; ?></td>
+				<td><?php verInput($conection, $id[$j]); ?>
+					<a onClick='if(confirm("¿Estas seguro que deseas borrar esta reputacion?")) location.href ="borrarReputacion.php?idReputacion=<?php echo $id[$j]; ?>";' tittle="eliminar" class="btn btn-default">
+						<img src="remove.png">
+					</a>
+				</td>
+				</tr>				
+			<?php
+				}
+			?>
+		</table>  
 	</div>
-</div>
+
+		<div style="text-align: center;">
+		<button class="btn btn-default" type="button" data-toggle="modal" data-target="#ModalAgregar">Agregar nueva Reputacion</button>
+		<div class="modal fade" id="ModalAgregar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	    <div class="modal-dialog" role="document">
+	    	<div class="modal-content">
+	            <div class="modal-header">
+	              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	              <h4 class="modal-title" id="myModalLabel">Agregar Reputacion</h4>
+	            </div>
+	            <div class="modal-body">
+		
+		<form class="form-horizontal" action="adminReputaciones.php" method="POST">
+		<div class="form-group">
+					<label class="control-label col-sm-2" for="color">Color:</label>
+					<div class="col-sm-10">
+						<input type="color" name="nuevoColor" class="form-control" required >
+					</div>
+		</div>
+			<br>
+				<div class="form-group">
+					<label class="control-label col-sm-2" for="nombre">Nombre:</label>
+					<div class="col-sm-10">
+						<input type="text" name="nuevoNombre" class="form-control" required >
+					</div>
+		</div>	
+			<br>
+				<div class="form-group">
+					<label class="control-label col-sm-2" for="valor_max">Valor maximo:</label>
+					<div class="col-sm-10">
+						<input type="number" max="9999" name="nuevoValor_max" class="form-control" required >
+						<b style="color: red;"> Se utilizara como valor minimo el de la reputacion anterior</b>
+					</div>
+		</div>
+			<br>
+			<input type="submit" class="btn btn-primary" name="Agregar" value="Agregar">
+		</form>
+		  </div>
+	            <div class="modal-footer">
+	              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+		</div>
+	</div>
+    </div>
+  </div>
+
+
+
 </div>
   
  
---
+++ modificarReputacion.php
@@ -0,0 +1,70 @@
+<?php  
+	function verInput($conection, $idReputacion){
+		$reputacion= $conection ->query("SELECT * FROM reputacion WHERE idReputacion='$idReputacion'");
+		$reputacion = $reputacion ->fetch_assoc();
+?>
+	<button title="modificar" class="btn btn-default" type="button" data-toggle="modal" data-target="#myModal<?php echo $reputacion['idReputacion']; ?>">
+		<img src="edit.png">
+	</button>
+	<div class="modal fade" id="myModal<?php echo $reputacion['idReputacion']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	    <div class="modal-dialog" role="document">
+	    	<div class="modal-content">
+	            <div class="modal-header">
+	              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	              <h4 class="modal-title" id="myModalLabel">Modificar Reputacion</h4>
+	            </div>
+	            <div class="modal-body">
+		
+		<form class="form-horizontal" action="adminReputaciones.php" method="POST">
+		<div class="form-group">
+					<label class="control-label col-sm-2" for="color">Color:</label>
+					<div class="col-sm-10">
+						<input type="color" name="color" value="<?php echo $reputacion['Color']; ?>" class="form-control" required >
+					</div>
+		</div>
+			<br>
+				<div class="form-group">
+					<label class="control-label col-sm-2" for="nombre">Nombre:</label>
+					<div class="col-sm-10">
+						<input type="text" name="nombre" value="<?php echo $reputacion['nombre']; ?>" class="form-control" required >
+					</div>
+		</div>	
+			<br>
+				<div class="form-group">
+					<label class="control-label col-sm-2" for="valor_max">Valor maximo:</label>
+					<div class="col-sm-10">
+						<input type="number" name="valor_max" max="9999" value="<?php echo $reputacion['valor_max']; ?>" class="form-control" required >
+						<b style="color: red;"> Se utilizara como valor minimo el de la reputacion anterior</b>
+					</div>
+		</div>
+			<br>
+			<input type="hidden" name="idReputacion" value="<?php echo $idReputacion; ?>">
+			<input type="submit" class="btn btn-primary" name="Modificar" value="Modificar">
+		</form>
+		  </div>
+	            <div class="modal-footer">
+	              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+<?php
+	}
+
+
+	function modificarReputacion($conection){
+		$nombre= $_POST['nombre'];
+		$color = $_POST['color'];
+		$valor_max= $_POST['valor_max'];
+		$idReputacion= $_POST['idReputacion'];
+		$existe= $conection-> query("SELECT * FROM reputacion WHERE idReputacion!=$idReputacion AND (nombre='$nombre' OR color='$color' OR valor_max='$valor_max')");
+		$existe = $existe->fetch_assoc();
+		if (!$existe) {
+			$valido=$conection->query("UPDATE reputacion SET nombre='$nombre', color='$color', valor_max='$valor_max' WHERE idReputacion=$idReputacion");
+		}
+		else{
+			return "Los datos ingresados no son validos, compruebe que no haya otra reputacion con los mismos";
+		}
+	}
+?>
+    
