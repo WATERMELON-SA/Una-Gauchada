f98a064d8f07789b47f736e79bd20c0d9db08aaa
santit96 <santi.tettamanti96@gmail.com>
2017-07-26 22:59:49 -0300

    Add files via upload

--- ganancias.php
+++ ganancias.php
@@ -2,29 +2,42 @@
 	include 'conexion.php';
 	include 'listador.php';
 	$mysql=conectar();
-	if (isset($_POST['boton'])){
-		$fecha_min=$_POST['fecha_min'];
-		$fecha_max=$_POST['fecha_max'];
-		$traer=$mysql->query("SELECT sum(cantidad) as creditos, sum(costo) as total FROM compra WHERE fecha_compra between '$fecha_min' and '$fecha_max' ");
-		$compra=$traer->fetch_assoc();
-		?>
-		<h3> Total ganancias:  $<?php echo $compra['total']; ?> </h3>
-		<h3> Creditos: <?php echo $compra['creditos']; ?> </h3>
-	</br>	
-	<h4>Detalle:</h4>
-	<div class="table-responsive">
-		<table class="table table-striped">
-			<tr >
-				<td style="font-weight: bold;">e-mail usuario</td>
-				<td style="font-weight: bold;">Cantidad de creditos</td>
-				<td style="font-weight: bold;">Costo</td>
-				<td style="font-weight: bold;">Fecha</td>
-			</tr>
-			<?php listarCompra(conectar(),$fecha_max,$fecha_min);  ?>
-		</table>
-	</div>
-		<?php
 
+	if (isset($_POST['boton'])){
+
+		if ($_POST['fecha_min'] > $_POST['fecha_max']){
+?>
+			<script type="text/javascript">
+				alert("La fecha inicio debe ser menor o igual a la fecha fin");
+
+			</script>
+<?php
+		}
+		else{	
+			$fecha_min=$_POST['fecha_min'];
+			$fecha_max=$_POST['fecha_max'];
+			$traer=$mysql->query("SELECT sum(cantidad) as creditos, sum(costo) as total FROM compra WHERE fecha_compra between '$fecha_min' and '$fecha_max' ");
+			$compra=$traer->fetch_assoc();
+?>			<br>
+			<h3 class="col-sm-7"> Total ganancias entre el <?php echo date("d-m-Y",strtotime($_POST['fecha_min'] ))  ?> y el <?php echo date("d-m-Y",strtotime($_POST['fecha_max'] ))  ?>: </h3>      
+			<h1 style="margin-top: 1%; color: green; font-weight:bold;">  $<?php echo $compra['total']; ?> </h1>   
+			
+	    	<br>	
+	    	<h4>Detalle:</h4>
+	    	<div class="table-responsive">
+				<table class="table table-striped">
+					<tr >
+						<td style="font-weight: bold;">e-mail usuario</td>
+						<td style="font-weight: bold;">Cantidad de creditos</td>
+						<td style="font-weight: bold;">Costo</td>
+						<td style="font-weight: bold;">Fecha</td>
+					</tr>
+					<?php listarCompra(conectar(),$fecha_max,$fecha_min);  ?>
+				</table>
+			</div>
+<?php
+
+		}
 	}
 
 
